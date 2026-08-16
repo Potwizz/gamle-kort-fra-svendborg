fc334c5ba2f83789d20e6f9e83f6ffa1c4588b52
Potwizz <Potwizz@pm.me>
2026-08-16 19:57:08 +0200

    version 1.0

--- Scenes/map_resource.gd
+++
@@ -1,8 +0,0 @@
-class_name MapData extends Resource
-
-## Refactor til stedpunkter.
-
-## button text for numbers?
-@export var button_text: String = ""
-@export_multiline var info_text: String = ""
-@export var pin_texture: Texture = null
